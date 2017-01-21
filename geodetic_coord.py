3168895a4150d47336cfd4f27230ee038a47a702
Anis Mohammed Vengasseri <anis.mhd@gmail.com>
2017-01-22 03:33:50 +0530

    First demo  problem added: Fixed bug in __repr__

--- geodetic_coord.py
+++ geodetic_coord.py
@@ -6,6 +6,7 @@ Program by
 
 	Started Date :: 20/01/2017
 '''
+from numpy import sqrt
 class geodetic_coord():
 	def __init__(self,latitude, longitude):
 		self.location = [latitude, longitude]
@@ -25,18 +26,30 @@ class geodetic_coord():
 		else:
 			longitude = float(str_longitude)
 		self.location = [latitude, longitude]
+	def distance_from_point(self, point):
+		R = 6371.0*(22.0/7.0)/180.0
+		return sqrt(sum((self.location - point.location)*R)**2)
 	def __repr__(self):
-		if latitude < 0:
-			str_latitude = "{0:9.4f} S".format(abs(self.latitude))
+		if self.location[0] < 0:
+			str_latitude = "{0:9.4f} S".format(abs(self.location[0]))
 		else:
-			str_latitude = "{0:9.4f} N".format(abs(self.latitude))
-		if longitude < 0:
-			str_longitude = "{0:9.4f} W".format(abs(self.longitude))
+			str_latitude = "{0:9.4f} N".format(abs(self.location[0]))
+		if self.location[1] < 0:
+			str_longitude = "{0:9.4f} W".format(abs(self.location[1]))
 		else:
-			str_longitude = "{0:9.4f} E".format(abs(self.longitude))
+			str_longitude = "{0:9.4f} E".format(abs(self.location[1]))
 		return "({0:s},{1:s})".format(str_latitude,str_longitude)
 '''
 Self test mechanism
 '''
 if __name__ == '__main__':
-	main()
+	print "\n\nThis is a python class for geodetic co-ordinate system ..."
+	print "\t Written by,"
+	print "\t\t Anis Mohammed Vengasseri"
+	print "\t\t anis.mhd@gmail.com"
+	print "\t\t https://github.com/anismhd\n\n"
+	print "\tTesing begin.."
+	print "Initialising one point in geodetic co-ordinate system (21.0,63.1)"
+	point1 = geodetic_coord(21.0,63.1)
+	print point1
+
