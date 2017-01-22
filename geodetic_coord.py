c88799b35a9f34559612154bc3c9a11f064fd258
Anis Mohammed Vengasseri <anis.mhd@gmail.com>
2017-01-22 16:09:44 +0530

    A new module for printing location in degree, minute and seconds format was attempter, later commented away for future takeup

--- geodetic_coord.py
+++ geodetic_coord.py
@@ -5,8 +5,10 @@ Program by
 	https://github.com/anismhd
 
 	Started Date :: 20/01/2017
+
+	This module requires more detailed section ... may be a new repository exclusively
 '''
-from numpy import sqrt
+from numpy import sqrt, array, floor
 class geodetic_coord():
 	def __init__(self,latitude, longitude):
 		if isinstance(latitude, str):
@@ -25,20 +27,35 @@ class geodetic_coord():
 					longitude = float(longitude.split()[0])
 			else:
 				longitude = float(longitude)
-		self.location = [latitude, longitude]
+		self.location = array([latitude, longitude])
 	def distance_from_point(self, point):
 		R = 6371.0*(22.0/7.0)/180.0
 		return sqrt(sum((self.location - point.location)*R)**2)
 	def __repr__(self):
-		if self.location[0] < 0:
+		if self.location[0] < 0.0:
 			str_latitude = "{0:9.4f} S".format(abs(self.location[0]))
 		else:
 			str_latitude = "{0:9.4f} N".format(abs(self.location[0]))
-		if self.location[1] < 0:
+		if self.location[1] < 0.0:
 			str_longitude = "{0:9.4f} W".format(abs(self.location[1]))
 		else:
 			str_longitude = "{0:9.4f} E".format(abs(self.location[1]))
 		return "({0:s},{1:s})".format(str_latitude,str_longitude)
+#	def str_in_degree_minute_seconds(self):
+#		deg_sign= u'\N{DEGREE SIGN}'
+#		latitude_degree = int(self.location[0])
+#		latitude_minute = int((self.location[0]-latitude_degree)*60)
+#		latitude_second = (self.location[0] - latitude_degree - (latitude_minute/60.0))*60.0
+#		longitude_degree = int(self.location[1])
+#		longitude_minute = int((self.location[1]-longitude_degree)*60)
+#		longitude_second = (self.location[1] - longitude_degree - (longitude_minute/60.0))*60.0
+#		dirc = ['N','E']
+#		if self.location[0] < 0.0:
+#			dirc[0] = 'S'
+#		if self.location[1] < 0.0:
+#			dirc[1] = 'W'
+#		return ("{0:2i}"+deg_sign+" {1:2i}\' {2:8.5f}\", {3:2i}"+deg_sign+" {4:2i}\' {5:8.5f}\"").format(\
+#			latitude_degree,latitude_minute,latitude_second,longitude_degree,longitude_minute, longitude_second)
 '''
 Self test mechanism
 '''
@@ -48,11 +65,16 @@ if __name__ == '__main__':
 	print "\t\t Anis Mohammed Vengasseri"
 	print "\t\t anis.mhd@gmail.com"
 	print "\t\t https://github.com/anismhd\n\n"
-	print "\tTesing begin.."
-	print "Demo 1 : Initialising one point in geodetic co-ordinate system (21.0,63.1)"
+	print "\tTesting begin.."
+	print "Demo 1 : Initializing point1 in geodetic co-ordinate system (21.0,63.1)"
 	point1 = geodetic_coord(21.0,63.1)
 	print point1
-	print "Initialising same point using string input 21.0 N, 63.1 E"
+	print "Initializing same point2 using string input 21.0 N, 63.1 E"
 	point2 = geodetic_coord('21.0 N', '63.1 E')
 	print point2
-
+	print "Distance between point1 and point2 using distance_from_point = {0:10.4f}\n".format(point1.distance_from_point(point2))
+	print "Defining location of various airport cities in India ...."
+	Delhi_airport = geodetic_coord
+	Mumbai
+	Chennai
+	Kolkata
