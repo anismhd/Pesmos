362c397297cd804234a0ec7d7f22dd7dc396afc0
Anis Mohammed Vengasseri <anis.mhd@gmail.com>
2017-01-25 00:13:30 +0530

    Prelimiary tests on earthquake event class

--- earthquake_event_class.py
+++ earthquake_event_class.py
@@ -48,11 +48,13 @@ class earthquake_event_class():
 		if 'mag_var' in kwarg.keys():
 			self.magnitude['mag_var'] = kwarg['mag_var']
 		''' Initialising self.location '''
-		self.location = {'location': None, 'loc_var': None}
+		self.location = {'location': None, 'loc_var': None, 'depth':None}
 		if ('latitude' in kwarg.keys()) and ('longitude' in kwarg.keys()):
 			self.location['location'] = geodetic_coord(kwarg['latitude'],kwarg['longitude'])
 		if 'loc_var' in kwarg.keys():
 			self.location['loc_var'] = kwarg['loc_var']
+		if 'depth' in kwarg.keys():
+			self.location['depth'] = kwarg['depth']
 	def eq_update_datetime(self, **kwarg):
 		if (('year' in kwarg.keys()) and ('month' in kwarg.keys())) and ('day' in kwarg.keys()):
 			if (('year' in kwarg.keys()) and ('month' in kwarg.keys())) and ('day' in kwarg.keys()):
@@ -65,6 +67,8 @@ class earthquake_event_class():
 			self.location['location'] = geodetic_coord(kwarg['latitude'],kwarg['longitude'])
 		if 'loc_var' in kwarg.keys():
 			self.location['loc_var'] = kwarg['loc_var']
+		if 'depth' in kwarg.keys():
+			self.location['depth'] = kwarg['depth']
 	def eq_update_name(self, name):
 		self.name = name
 	def eq_update_magnitude(self, **kwarg):
@@ -78,5 +82,48 @@ class earthquake_event_class():
 		self.region = region
 	def eq_update_note(self, note):
 		self.note = note
+	# String maniplation, operation overloaded
+	def __repr__(self):
+		if self.name:
+			string = "\t{0:20s} : {1:s} \n".format('Name',self.name)
+		else:
+			string = "\t{0:20s} : [None] \n".format('Name')
+		if self.region:
+			string = string + "\t{0:20s} : {1:s} \n".format('Region',self.region)
+		else:
+			string = string + "\t{0:20s} : [None] \n".format('Region')
+		if self.datetime:
+			string = string + "\t{0:20s} : {1:s} \n".format('Date',self.datetime)
+		else:
+			string = string + "\t{0:20s} : [None] \n".format('Date')
+		if self.magnitude['mag']:
+			string = string + "\t{0:20s} : {1:4.2f}".format('Magnitude',self.magnitude['mag'])
+		else:
+			string = string + "\t{0:20s} : [None]".format('Magnitude')
+		if self.magnitude['mag_var']:
+			string = string + " error {1:6.3f}".format(self.magnitude['mag_var'])
+		else:
+			string = string + " error [None]"
+		if self.magnitude['mag_type']:
+			string = string + " {1:s}\n".format(self.magnitude['mag_type'])
+		else:
+			string = string + " [None]\n"
+		if self.location['location']:
+			string = string + "\t{0:20s} : {1:s}".format('Location',str(self.location['location']))
+		else:
+			string = string + "\t{0:20s} : [None]".format('Location')
+		if self.location['loc_var']:
+			string = string + " erros {0:f}\n".format(self.location['loc_var'])
+		else:
+			string = string + " erros [None]\n"
+		if self.location['depth']:
+			string = string + "\t{0:20s} : {1:6.2f}".format('Depth',self.location['depth'])
+		else:
+			string = string + "\t{0:20s} : [None]".format('Depth')
+		return string
 if __name__ == '__main__':
+	'''
+	This is a self test for earthquake_event_class
+	'''
 	event1 = earthquake_event_class()
+	print event1
