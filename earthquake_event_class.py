ae7f03466620ad20c16d304b12a5ddcabd6506be
Anis Mohammed Vengasseri <anis.mhd@gmail.com>
2017-01-24 19:42:20 +0530

    ADDD - No idea

---
+++ earthquake_event_class.py
@@ -0,0 +1,15 @@
+'''
+Program by
+	Anis Mohammed Vengasseri
+	anis.mhd@gmail.com
+	https://github.com/anismhd
+
+	Started Date :: 23/01/2017
+
+	This module requires more detailed section ... may be a new repository exclusively
+'''
+from timedate import 
+class earthquake_event_class():
+	"""An earthquake event class"""
+	def __init__(self, Name, time, mag = None, location=None, mag_type = None):
+		self.arg = arg
